094b0696293f23c4c31bab7cd0a46754c1b1bb5d
Atsushi Tadokoro <tadokoro@gmail.com>
2017-08-17 11:31:45 +0900

    update

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -1,26 +1,26 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
 	scsynth = new ofxSCSynthServer();
-    
-    //boot scsynth
+
+	//boot scsynth
 	scsynth->boot();
-    //wait for boot...
+	//wait for boot...
 	ofSleepMillis(5000);
-    //load synthdef files
+	//load synthdef files
 	scsynth->loadSynthDefsDir();
-    //OSC setup
-    sender.setup("127.0.0.1", 57110);
+	//OSC setup
+	sender.setup("127.0.0.1", 57110);
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
 
 }
 
@@ -29,72 +29,72 @@ void ofApp::exit() {
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::keyPressed(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-    if (key == 'a')	{
-        //start synth
-        ofxOscMessage m;
-        m.setAddress("/s_new");
-        m.addStringArg("sine");
-        m.addInt32Arg(1000);
-        m.addInt32Arg(0);
-        m.addInt32Arg(0);
-        sender.sendMessage(m);
-    }
-    if (key == 's')	{
-        //stop synth
-        ofxOscMessage m;
-        m.setAddress("/n_free");
-        m.addInt32Arg(1000);
-        sender.sendMessage(m);
-    }
+void ofApp::keyReleased(int key) {
+	if (key == 'a') {
+		//start synth
+		ofxOscMessage m;
+		m.setAddress("/s_new");
+		m.addStringArg("sine");
+		m.addInt32Arg(1000);
+		m.addInt32Arg(0);
+		m.addInt32Arg(0);
+		sender.sendMessage(m);
+	}
+	if (key == 's') {
+		//stop synth
+		ofxOscMessage m;
+		m.setAddress("/n_free");
+		m.addInt32Arg(1000);
+		sender.sendMessage(m);
+	}
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::mouseExited(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
